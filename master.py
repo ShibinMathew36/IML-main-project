acbf395bed5eed8f3380f69975fa0de78707f625
ShibinMathew36 <smathe36@uic.edu>
2018-11-22 00:32:58 -0600

    fetches context terms

--- master.py
+++ master.py
@@ -1,3 +1,4 @@
+import gc
 import glob
 import string
 from multiprocessing import Pool
@@ -12,23 +13,25 @@ train_neg_path = "/home/jarvis/Documents/iml/Project/DataSets/Imdb_dataset/train
 test_pos_path = "/home/jarvis/Documents/iml/Project/DataSets/Imdb_dataset/test/pos/"
 test_neg_path = "/home/jarvis/Documents/iml/Project/DataSets/Imdb_dataset/test/neg/"
 vectorizer_pos = CountVectorizer()
-#vectorizer_neg = CountVectorizer()
+vectorizer_neg = CountVectorizer()
 pos_key_terms = []
-key_term_occurance_pos = [[]]
-key_term_occurance_neg = [[]]
+key_term_occurance_pos = []
+key_term_occurance_neg = []
 context_terms = []
 
 
 # Constants used:
 # TODO: all values are dummy, need to figure out actuals
-key_term_threshold = 2
+key_term_threshold = 5
 context_span = 3
-context_term_val_threshold = 1
+context_term_val_threshold = 0
 
 def get_data(path):
     data = []
     temp = 0
     for files in glob.glob(path + "*.txt"):
+        if temp == 100:
+            break
         infile = open(files)
 
         #fix case and remove punctuations, nunbers
@@ -50,6 +53,31 @@ def get_data(path):
         temp += 1
     return data
 
+def get_key_terms(X_train, Y_train,  prim_freq, sec_freq, pos_neg_ratio):
+    # TODO: include code to do dirichlet smoothing
+    prim_train = np.array(X_train.toarray())
+    sec_train = np.array(Y_train.toarray())
+    temp = 0
+    prim_key_terms = []
+    prim_term_occurance = []
+    sec_term_occurance = []
+
+    for key in prim_freq.keys():
+        temp_sec_freq = 1
+        flag = False
+        if key in sec_freq.keys():
+            temp_sec_freq = sec_freq[key]
+            flag = True
+        if (prim_freq[key] / temp_sec_freq) * pos_neg_ratio > key_term_threshold:
+            prim_key_terms.append(key)
+            prim_term_occurance.append(np.flatnonzero(prim_train[:, temp]).tolist())
+            if flag:
+                sec_term_occurance.append(np.flatnonzero(sec_train[:, list(sec_freq.keys()).index(key)]).tolist())
+            else:
+                sec_term_occurance.append([])
+        temp += 1
+    return prim_key_terms, prim_term_occurance, sec_term_occurance
+
 
 def get_context_dict(key_terms, key_term_occurance, train):
     context_dict = {}
@@ -61,8 +89,8 @@ def get_context_dict(key_terms, key_term_occurance, train):
                 position = review.index(key)
                 start = max(0, position - context_span)
                 end = min(len(review), (position + context_span + 1))
-                subsring = review[start:position] + review[(position + 1):end]
-                for word in subsring:
+                substring = review[start:position] + review[(position + 1):end]
+                for word in substring:
                     if word in context_dict:
                         context_dict[word] += 1
                         count += 1
@@ -72,60 +100,70 @@ def get_context_dict(key_terms, key_term_occurance, train):
     return context_dict, count
 
 
+def get_context_terms(context_prim, context_sec, context_prim_count, context_sec_count):
+    context_terms = []
+    for val in context_prim.keys():
+        neg_freq = 0 # is the ideal initializer ?
+        if val in context_sec:
+            neg_freq = context_sec[val]
+        #print ((context_prim[val] / context_prim_count) - (neg_freq / context_sec_count), end="  ")
+        if ((context_prim[val] / context_prim_count) - (neg_freq / context_sec_count)) >= context_term_val_threshold:
+            context_terms.append(val)
+    return context_terms
+
 if __name__ == '__main__':
 
-    with Pool(4) as p:
-        [train_positive, train_negative, test_positive, test_negative] = \
-            p.map(get_data, [train_pos_path, train_neg_path, test_pos_path, test_neg_path])
-
+    p = Pool(4)
+    [train_positive, train_negative, test_positive, test_negative] = \
+        p.map(get_data, [train_pos_path, train_neg_path, test_pos_path, test_neg_path])
+    gc.collect()
     print("\n Data extracting and preprocessing done. Processed review count = ", str(4 * len(train_positive)))
     print ("\n Training :")
     X_train = vectorizer_pos.fit_transform(train_positive)
-    Y_train = vectorizer_pos.transform(train_negative) # should we fit it with same or different classifier objects ?
+    Y_train = vectorizer_neg.fit_transform(train_negative) # should we fit it with same or different classifier objects ?
     train_pos = X_train.toarray().sum(axis=0)
     train_neg = Y_train.toarray().sum(axis=0)
-
     print ("\n  Features extracted.")
     print ("Total positive words = ", train_pos.sum())
     print ("Total negative words = ", train_neg.sum()) # will this be a problem since i train with same classifier
 
     pos_neg_ratio = float(train_neg.sum() / train_pos.sum())
     pos_features = col.OrderedDict(zip(vectorizer_pos.get_feature_names(), train_pos))
-    neg_features = col.OrderedDict(zip(vectorizer_pos.get_feature_names(), train_neg))
+    neg_features = col.OrderedDict(zip(vectorizer_neg.get_feature_names(), train_neg))
 
     print ("\n  Extracting key words.")
-    # TODO: include code to do dirichlet smoothing
-    temp = 0
-    np_Xtrain = np.array(X_train.toarray())
-    np_Ytrain = np.array(Y_train.toarray())
-
-    for key in pos_features.keys():
-        temp = max(1, neg_features[key])
-        if (pos_features[key] / temp) * pos_neg_ratio > key_term_threshold:
-            pos_key_terms.append(key)
-            key_term_occurance_pos.append(np.flatnonzero(np_Xtrain[:, temp]).tolist())
-            key_term_occurance_neg.append(np.flatnonzero(np_Ytrain[:, temp]).tolist())
-        temp += 1
-
+    gc.collect()
+    pos_key_terms, key_occurrance_pp, key_occurrance_pn = get_key_terms(X_train, Y_train, pos_features, neg_features, pos_neg_ratio)
+    neg_key_terms, key_occurrance_nn, key_occurrance_np = get_key_terms(Y_train, X_train, neg_features, pos_features, pos_neg_ratio)
     print (str(len(pos_key_terms)), " positive key words extracted.")
-
+    print (str(len(neg_key_terms)), " negative key words extracted.")
     print ("\n  Extracting context terms.")
-    # TODO: parallelize this code, can go faster
-    context_positive, context_positive_count = get_context_dict(pos_key_terms, key_term_occurance_pos, train_positive)
-    context_negative, context_negative_count = get_context_dict(pos_key_terms, key_term_occurance_neg, train_negative)
-
-    for val in context_positive.keys():
-        neg_freq = 0
-        if val in context_negative:
-            neg_freq = context_negative[val]
-        if ((context_positive[val] / context_positive_count) - (neg_freq / context_negative_count)) >= \
-                context_term_val_threshold: # not just frequency ??
-            context_terms.append(val)
-
-    print ("Extracted ", str(len(context_terms)), " positive context terms.")
-
-    print ("\n Testing :")
     
-    """X_test = vectorizer_pos.transform(test_positive)
+    # TODO: parallelize this code, can go faster
+    # extracting context term occurrences and count
+    context_pp, context_pp_count = get_context_dict(pos_key_terms, key_occurrance_pp, train_positive)
+    print (len(context_pp), context_pp_count)
+    context_pn, context_pn_count = get_context_dict(pos_key_terms, key_occurrance_pn, train_negative)
+    print (len(context_pn), context_pn_count)
+    context_nn, context_nn_count = get_context_dict(neg_key_terms, key_occurrance_nn, train_negative)
+    print (len(context_nn), context_nn_count)
+    context_np, context_np_count = get_context_dict(neg_key_terms, key_occurrance_np, train_positive)
+    print (len(context_np), context_np_count)
+    key_occurrance_pp.clear(); key_occurrance_pn.clear(); key_occurrance_nn.clear(); key_occurrance_np.clear
+    gc.collect()
+    # extracting context terms
+    pos_context_terms = get_context_terms(context_pp, context_pn, context_pp_count, context_nn_count)
+    print ("\n \n")
+    neg_context_terms = get_context_terms(context_nn, context_np, context_nn_count, context_pp_count)
+    context_pp.clear(); context_pn.clear(); context_nn.clear(); context_np.clear()
+    gc.collect()
+
+    print ("Extracted ", str(len(pos_context_terms)), " positive context terms.")
+    print (pos_context_terms[:200])
+    print ("Extracted ", str(len(neg_context_terms)), " negative context terms.")
+    print (neg_context_terms[:200])
+    print ("\n Testing :")
+    gc.collect()
+    X_test = vectorizer_pos.transform(test_positive)
     print(X_test.toarray().sum(axis=0))
-    """
+    
